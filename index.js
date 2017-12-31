dc646222cecfea8e907ef280a8a8a33e66f565e5
Tyler Maxwell <tylermaxwell661@gmail.com>
2017-12-31 18:24:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li ');
+  for(let i =0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML * n);
+  }
+}
+
+function deepestChild() {
+  
+}
