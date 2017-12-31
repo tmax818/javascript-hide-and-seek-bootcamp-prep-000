6f90e2dce06db4038323c7443c4e9f9d6f2b224d
Tyler Maxwell <tylermaxwell661@gmail.com>
2017-12-31 18:25:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const lis = document.querySelectorAll('ul.ranked-list li ');
+  const lis = document.querySelectorAll('.ranked-list li ');
   for(let i =0; i < lis.length; i++) {
     lis[i].innerHTML = parseInt(lis[i].innerHTML * n);
   }
